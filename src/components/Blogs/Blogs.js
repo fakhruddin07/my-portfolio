ba8328c4f0c889e5ccf4262b158a1571f6276ab8
fakhruddin07 <fakhruddinnoman95@gmail.com>
2021-05-03 02:28:37 +0600

    edit blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import chanderHut from '../../images/NOMAN.jpg';
+import chanderHut from '../../images/NOMAN.jpg'
 
 const Blogs = () => {
     return (
@@ -12,8 +12,8 @@ const Blogs = () => {
                             <div class="card">
                                 <img src={chanderHut} class="card-img-top" alt="..." />
                                 <div class="card-body text-center">
-                                    <h5 class="card-title">Chander Hut</h5>
-                                    <a href="#" class="btn btn-primary mb-3">Read</a>
+                                    <h5 class="card-title">The Mistake I Made As a Beginner</h5>
+                                    <a href="https://link.medium.com/fUVNeEtiWfb?fbclid=IwAR2d7284lzlYl1SsyIyaAWoaXsBSD_32dQWYEdF8xzCSFyo8z7fbecxcPSI" class="btn btn-primary mb-3">Read</a>
                                 </div>
                             </div>
                         </div>
@@ -21,8 +21,8 @@ const Blogs = () => {
                             <div class="card">
                                 <img src={chanderHut} class="card-img-top img-fluid" alt="..." />
                                 <div class="card-body text-center">
-                                    <h5 class="card-title ">Repair Hut</h5>
-                                    <a href="#" class="btn btn-primary mb-3">Read</a>
+                                    <h5 class="card-title ">How to be a better react Developer</h5>
+                                    <a href="https://link.medium.com/QvXm6nyjWfb?fbclid=IwAR25cHfGs35pkDd5bKOUmO5lCcftuo9Zs9h1vH6duehnK5NtDmrmoKNeLoE" class="btn btn-primary mb-3">Read</a>
                                 </div>
                             </div>
                         </div>
@@ -30,8 +30,8 @@ const Blogs = () => {
                             <div class="card">
                                 <img src={chanderHut} class="card-img-top" alt="..." />
                                 <div class="card-body text-center">
-                                    <h5 class="card-title">City Ride</h5>
-                                    <a href="#" class="btn btn-primary mb-3">Read</a>
+                                    <h5 class="card-title">Learning How to Learn JavaScript</h5>
+                                    <a href="https://link.medium.com/SpSOY0UjWfb?fbclid=IwAR2ZgZDp4KQedYh0s1cqMq7QHeDQP8BnU7oG7XYqrLLdQ47awvmSyv5gRFU" class="btn btn-primary mb-3">Read</a>
                                 </div>
                             </div>
                         </div>
